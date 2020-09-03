86cb638921607f276398abe3d51a5ee8e994feef
Jordan Morehead <jmorehead@zagmail.gonzaga.edu>
2020-09-02 18:34:35 -0700

    Initial Commit

---
+++ You Are Awesome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//
+//  Created by Jordan Morehead on 9/2/20.
+//  Copyright © 2020 Jordan Morehead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
